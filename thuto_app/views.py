5b7c6ef25bc2ba76420eadc372dd74c9f16f0876
tokelomakafane <tokelomakafane1@gmail.com>
2025-07-10 20:01:45 +0200

    noo

--- thuto_app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def welcome(request):
-    """View that displays welcome message to Thuto"""
-    return render(request, 'thuto_app/welcome.html')
